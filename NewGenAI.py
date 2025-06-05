68f82fe3afd500fa4d122618e44eb5d6d0d77c27
adarshsharma3 <adarshsharma300704@gmail.com>
2025-06-05 13:40:07 +0530

    Changed

--- NewGenAI.py
+++ NewGenAI.py
@@ -3,7 +3,7 @@
 # load_dotenv()
 # from google import genai
 
-# # client = genai.Client(api_key=AIzaSyDdnvrUJ9xPs2dT0m2MR_--o1i7oi96lus))
+# # client = genai.Client(api_key=AIzaSy))
 # client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 # response = client.models.generate_content(
 #     model="gemini-2.0-flash",
